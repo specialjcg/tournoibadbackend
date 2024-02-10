52a19e7903328a4d148b86c756d71681a5180a22
Jean-Charles GOULEAU <jean-charles.gouleau@savoye.com>
2024-02-10 18:13:21 +0100

    set a post to store random teams

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,165 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::{Request, Response};
+use std::string::ToString;
+use rocket::{Data, Request, Response};
 use rand::seq::SliceRandom;
+use rocket::data::FromData;
 use rocket::fairing::{Fairing, Info, Kind};
-use rocket::http::Header;
-use rocket::serde::json::Json;
+use rocket::futures::AsyncReadExt;
+use rocket::http::{Header, Status};
+use rocket::serde::json::{Json, serde_json};
+use rocket::yansi::Paint;
+use rocket_cors::{AllowedOrigins, CorsOptions, Method};
+use serde::__private::de::IdentifierDeserializer;
 
 
-#[derive(Debug, serde::Serialize, Clone)]
+#[derive(Debug, serde::Serialize, Clone,serde::Deserialize,PartialEq)]
 struct Task {
     name: String,
     completed: bool,
 }
 
-#[derive(Debug, serde::Serialize)]
+#[derive(Debug, serde::Serialize,Clone,PartialEq)]
 struct TaskList {
     tasks: Vec<Task>,
 }
+fn create_task_list() -> TaskList {
+    TaskList {
+        tasks: vec![
+            Task { name: String::from("Les Monstres de Marshmallow"), completed: false },
+            Task { name: String::from("Les Télégraphistes de la Galaxie"), completed: false },
+            Task { name: String::from("Les Centaures Turbulents"), completed: false },
+            Task { name: String::from("Les Slalomeurs de la Fantaisie"), completed: false },
+            Task { name: String::from("Les Lutins Affamés"), completed: false },
+            Task { name: String::from("Les Strangers Flamboyants"), completed: false },
+            Task { name: String::from("Les Cosaques de l’Ouest"), completed: false },
+            Task { name: String::from("Les Épéistes du Royaume"), completed: false },
+            Task { name: String::from("Les Patineurs de la Révolution"), completed: false },
+            Task { name: String::from("Les Gladiateurs du Temps"), completed: false },
+            Task { name: String::from("Les Grenouilles de l’Écume"), completed: false },
+            Task { name: String::from("Les Vagabonds de l’Espace"), completed: false },
+            Task { name: String::from("Les Cœurs Héroïques"), completed: false },
+            Task { name: String::from("Les Bandits de l’Aurore"), completed: false },
+            Task { name: String::from("Les Mousquetaires des Ténèbres"), completed: false },
+            Task { name: String::from("Les Éclaireurs de l’Avenir"), completed: false },
+            Task { name: String::from("Les Chevaliers de l’Hiver"), completed: false },
+            Task { name: String::from("Les Fous Volants de la Nuit"), completed: false },
+            Task { name: String::from("Les Sorciers de l’Orient"), completed: false },
+            Task { name: String::from("Les Hiérarques de la Paix"), completed: false },
+            Task { name: String::from("Les Cavaliers du Vent"), completed: false },
+            Task { name: String::from("Les Cavaliers du Destin"), completed: false },
+        ],
+    }
+}
 
-#[get("/tasks")]
-fn get_tasks() -> Json<TaskList> {
+#[get("/teams")]
+fn get_teams() -> Json<TaskList> {
     // Create a list of tasks similar to the Java code
-    let task_list = vec![
-        Task { name: String::from("Les Monstres de Marshmallow"), completed: false },
-        Task { name: String::from("Les Télégraphistes de la Galaxie"), completed: false },
-        Task { name: String::from("Les Centaures Turbulents"), completed: false },
-        Task { name: String::from("Les Slalomeurs de la Fantaisie"), completed: false },
-        Task { name: String::from("Les Lutins Affamés"), completed: false },
-        Task { name: String::from("Les Étrangers Flamboyants"), completed: false },
-        Task { name: String::from("Les Cosaques de l’Ouest"), completed: false },
-        Task { name: String::from("Les Épéistes du Royaume"), completed: false },
-        Task { name: String::from("Les Patineurs de la Révolution"), completed: false },
-        Task { name: String::from("Les Gladiateurs du Temps"), completed: false },
-        Task { name: String::from("Les Grenouilles de l’Écume"), completed: false },
-        Task { name: String::from("Les Vagabonds de l’Espace"), completed: false },
-        Task { name: String::from("Les Cœurs Héroïques"), completed: false },
-        Task { name: String::from("Les Bandits de l’Aurore"), completed: false },
-        Task { name: String::from("Les Mousquetaires des Ténèbres"), completed: false },
-        Task { name: String::from("Les Éclaireurs de l’Avenir"), completed: false },
-        Task { name: String::from("Les Chevaliers de l’Hiver"), completed: false },
-        Task { name: String::from("Les Fous Volants de la Nuit"), completed: false },
-        Task { name: String::from("Les Sorciers de l’Orient"), completed: false },
-        Task { name: String::from("Les Hiérarques de la Paix"), completed: false },
-        Task { name: String::from("Les Cavaliers du Vent"), completed: false },
-        Task { name: String::from("Les Cavaliers du Destin"), completed: false },
-    ];
+
 
     // Shuffle the tasks randomly
     let mut rng = rand::thread_rng();
-    let mut selected_tasks = task_list.choose_multiple(&mut rng, 5).cloned().collect::<Vec<Task>>();
+    let mut selected_tasks = create_task_list().tasks.choose_multiple(&mut rng, 5).cloned().collect::<Vec<Task>>();
 
     // Create and return the TaskList struct
     let task_list_response = TaskList { tasks: selected_tasks };
     Json(task_list_response)
 }
-pub struct Cors;
 
-#[rocket::async_trait]
-impl Fairing for Cors {
-    fn info(&self) -> Info {
-        Info {
-            name: "Cross-Origin-Resource-Sharing Fairing",
-            kind: Kind::Response,
-        }
-    }
+#[post("/teamspost", format = "text", data = "<user_input>")]
+fn hello_post(user_input: String) -> Result<String, Status> {
+    let task: String = user_input;
+    println!("Received data: {:?}", task.to_string());
+    Ok("Data received successfully".to_string())
 
-    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new(
-            "Access-Control-Allow-Methods",
-            "POST, PATCH, PUT, DELETE, HEAD, OPTIONS, GET",
-        ));
-        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-    }
 }
+fn cors() -> rocket_cors::Cors {
+    // Define allowed origins
+    let allowed_origins = AllowedOrigins::all(); // Allow requests from any origin
+
+    // Create CORS options
+    let cors = CorsOptions {
+        allowed_origins,
+        allowed_methods: vec![rocket::http::Method::Get, rocket::http::Method::Post, rocket::http::Method::Options] // Add other methods as needed
+            .into_iter()
+            .map(From::from)
+            .collect(),
+        allow_credentials: true,
+        ..Default::default()
+    }
+        .to_cors()
+        .expect("Error while building CORS");
+
+    cors
+}
+
 #[launch]
 fn rocket() -> _ {
+
+
+
     rocket::build()
         .configure(rocket::Config::figment().merge(("port", 4000)))
-        .attach(Cors)
-        .mount("/api", routes![get_tasks])
+        .mount("/api", routes![get_teams,hello_post])
+        .attach(cors())
+
+
+}
+impl ToString for Task {
+    fn to_string(&self) -> String {
+        format!("{{ name: {}, completed: {} }}", self.name, self.completed)
+    }
+}
+fn json_to_tasklist(json_str: &str) -> Result<TaskList, serde_json::Error> {
+    // Parse the JSON string into a vector of Task objects
+    let tasks: Vec<Task> = serde_json::from_str(json_str)?;
+
+    // Create a TaskList from the vector of Task objects
+    let task_list = TaskList { tasks };
+
+    Ok(task_list)
 }
 
 
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        let teststring=Task { name: String::from("Les Monstres de Marshmallow"), completed: false };
+        assert_eq!(hello_post(teststring.to_string()), Ok("Data received successfully".to_string()));
+    }
+    #[test]
+    fn test_transform_to_task_list() {
+        // Define teststring as a string literal with a static lifetime
+        let json_str = r#"[{ "name": "Les Monstres de Marshmallow", "completed": false },{ "name": "Les Monstres de Marshmallow", "completed": false }]"#;
+
+
+        // Extract the inner values for comparison
+        let expected_result = vec![
+            Task { name: String::from("Les Monstres de Marshmallow"), completed: false },
+            Task { name: String::from("Les Monstres de Marshmallow"), completed: false }
+        ];
+        let task_list_result = json_to_tasklist(json_str);
+
+        match task_list_result {
+            Ok(task_list) => {
+                println!("TaskList: {:?}", task_list);
+                assert_eq!(task_list, TaskList { tasks: expected_result });//, "Expected: {:?}, Actual: {:?}", expected_result);
+            }
+            Err(e) => {
+                println!("Error parsing JSON: {}", e);
+            }
+        }
+
+        // Assert that the inner values match
+
+    }
+
+
+}
 
 
